c09bee93f250db11d47df473f70cf26d48af4b69
rusev9 <evg.rusev@gmail.com>
2018-10-15 02:40:17 +0300

    expand class functionallity

--- http-server-request-response-handler/src/server/RequestHandler.java
+++ http-server-request-response-handler/src/server/RequestHandler.java
@@ -3,8 +3,14 @@ package server;
 import server.http.HttpRequest;
 import server.http.HttpRequestImpl;
 import server.http.HttpResponse;
+import server.http.HttpStatus;
+
+import java.io.File;
 
 public final class RequestHandler {
+    private static final String PAGES_PATH = "C:\\Users\\Evgeni\\IdeaProjects\\java-web-fundamentals\\http-server-request-response-handler\\src\\resources\\pages\\";
+    private static final String HOME_PAGE_NAME = "index.html";
+    private static final String ROOT_REQUEST = "/";
     private HttpRequest httpRequest;
     private HttpResponse httpResponse;
 
@@ -25,17 +31,37 @@ public final class RequestHandler {
     }
 
     private byte[] processGetRequest() {
+        String url = this.httpRequest.getRequestUrl();
+
         if (this.httpRequest.isResource()) {
-            return this.processResourceRequest();
+            return this.processResourceRequest(url);
         }
-        return this.processPageRequest();
+
+        return this.processPageRequest(url);
     }
 
-    private byte[] processPageRequest() {
-        // TODO
+    private byte[] processPageRequest(String url) {
+        if (ROOT_REQUEST.equals(url)) {
+            this.httpRequest.setRequestUrl(HOME_PAGE_NAME);
+        }
+
+        String pagePath = PAGES_PATH + this.httpRequest.getRequestUrl();
+
+        File page = new File(pagePath);
+
+        if (!page.exists() || page.isDirectory()) {
+            return this.pageNotFound();
+        }
+
+        this.httpResponse.setStatusCode(HttpStatus.OK);
+        // TODO implement the rest of the response headers
     }
 
-    private byte[] processResourceRequest() {
+    private byte[] pageNotFound() {
+
+    }
+
+    private byte[] processResourceRequest(String url) {
         // TODO
     }
 }
