bb327a528376b05decae02cd3274d7bc27b109ed
rusev9 <evg.rusev@gmail.com>
2018-10-22 17:23:17 +0300

    merge web-server Javache

--- http-server-from-scratch/src/server/WebConstants.java
+++
@@ -1,8 +0,0 @@
-package server;
-
-public final class WebConstants {
-    public static final Integer PORT = 8007;
-    public  static final Integer SOCKET_TIMEOUT_MILLISECONDS = 5000;
-    public static final String EMPTY_STRING = "";
-    public static final String HTTP_PROTOCOL_1 = "HTTP/1.1";
-}
