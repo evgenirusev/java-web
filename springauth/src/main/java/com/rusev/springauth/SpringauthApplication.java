4610142b9a5c1a6f49edd311622081eb0897e922
rusev9 <evg.rusev@gmail.com>
2018-11-24 16:08:25 +0200

    add new spring boot project

---
+++ springauth/src/main/java/com/rusev/springauth/SpringauthApplication.java
@@ -0,0 +1,12 @@
+package com.rusev.springauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringauthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringauthApplication.class, args);
+    }
+}
