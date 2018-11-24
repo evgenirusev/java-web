17606a9b09e04e634acdc76d869fd7b176e02a8e
rusev9 <evg.rusev@gmail.com>
2018-11-24 16:32:37 +0200

    Home, Head fragment HomeController

---
+++ springauth/src/main/java/com/rusev/springauth/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package com.rusev.springauth.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController extends BaseController {
+    @GetMapping("/")
+    public ModelAndView home(ModelAndView modelAndView) {
+        return super.view("home");
+    }
+}
