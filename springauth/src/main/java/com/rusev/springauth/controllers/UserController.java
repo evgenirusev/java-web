d372c1f21a69896f71d5d8c3e7e17104ba6cd229
rusev9 <evg.rusev@gmail.com>
2018-11-25 11:07:51 +0200

    User entity, UserRegisterBindingModel

--- springauth/src/main/java/com/rusev/springauth/controllers/UserController.java
+++ springauth/src/main/java/com/rusev/springauth/controllers/UserController.java
@@ -1,7 +1,10 @@
 package com.rusev.springauth.controllers;
 
+import com.rusev.springauth.models.binding.UserRegisterBindingModel;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -12,5 +15,11 @@ public class UserController extends BaseController {
     @GetMapping("/register")
     public ModelAndView register() {
         return super.view("register");
+    }
+
+    @PostMapping("/register")
+    public ModelAndView storeRegister(@ModelAttribute UserRegisterBindingModel userRegisterBindingModel) {
+
+        return null;
     }
 }
---
+++ springauth/src/main/java/com/rusev/springauth/entities/User.java
@@ -0,0 +1,73 @@
+package com.rusev.springauth.entities;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    private String id;
+
+    private String username;
+
+    private String password;
+
+    private String confirmPassword;
+
+    private String email;
+
+    public User() {
+    }
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+            name = "UUID",
+            strategy = "org.hibernate.id.UUIDGenerator"
+    )
+    @Column(name = "id", nullable = false, unique = true, updatable = false)
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Column(name = "username", nullable = false, unique = true)
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Column(name = "password", nullable = false)
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+
+    @Column(name = "email")
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
