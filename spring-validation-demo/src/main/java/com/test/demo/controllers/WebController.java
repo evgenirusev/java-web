c0b21bc24246ccd001cdd36a76b06d1969a09bb0
rusev9 <evg.rusev@gmail.com>
2018-12-03 11:33:12 +0200

    Validation Demo

---
+++ spring-validation-demo/src/main/java/com/test/demo/controllers/WebController.java
@@ -0,0 +1,37 @@
+package com.test.demo.controllers;
+
+import javax.validation.Valid;
+
+import com.test.demo.models.dto.persons.PersonForm;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Controller
+public class WebController implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/results").setViewName("results");
+    }
+
+    @GetMapping("/")
+    public String showForm(PersonForm personForm) {
+        return "form";
+    }
+
+    @PostMapping("/")
+    public String checkPersonInfo(@Valid PersonForm personForm, BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            System.out.println("Validation errors!");
+            return "form";
+        }
+
+        return "redirect:/results";
+    }
+}
